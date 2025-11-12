003eb01c83adda08a0192e6f809a39a91d95cafd
LCP05 <lucas.previtero@sptech.school>
2025-11-12 00:49:35 -0300

    Atualização da parte de contas do site

--- src/controllers/contasController.js
+++ src/controllers/contasController.js
@@ -10,6 +10,8 @@ async function listar(req, res) {
   }
 }
 
+
+
 async function listarHospital(req, res) {
   const { idHospital } = req.params;
 
@@ -34,12 +36,14 @@ async function listarEmpresa(req, res) {
   }
 }
 
-async function alterarAcesso(req, res) {
+
+
+async function alterarAcessoHospital(req, res) {
   const { statusUser } = req.body;
   const { id } = req.params;
 
   try {
-    await contasModel.alterarAcesso(id, statusUser);
+    await contasModel.alterarAcessoHospital(id, statusUser);
     res.json({ msg: "Status alterado com sucesso" });
   } catch (err) {
     console.error(err);
@@ -47,12 +51,40 @@ async function alterarAcesso(req, res) {
   }
 }
 
-async function editarUsuario(req, res){
+async function alterarAcessoEmpresa(req, res) {
+  const { statusUser } = req.body;
+  const { id } = req.params;
+
+  try {
+    await contasModel.alterarAcessoEmpresa(id, statusUser);
+    res.json({ msg: "Status alterado com sucesso" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Erro ao alterar status");
+  }
+}
+
+
+
+
+async function editarContaEmpresa(req, res){
   const id = req.params.id;
   const { nome, email, perfil} = req.body;
 
   try {
-    const resultado = await contasModel.editarUsuario(id, nome, email, perfil);
+    const resultado = await contasModel.editarContaEmpresa(id, nome, email, perfil);
+    res.json({ message: "Usuário atualizado com sucesso" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erro ao editar usuário" });
+  }
+}
+async function editarContaHospital(req, res){
+  const id = req.params.id;
+  const { nome, email, perfil} = req.body;
+
+  try {
+    const resultado = await contasModel.editarContaHospital(id, nome, email, perfil);
     res.json({ message: "Usuário atualizado com sucesso" });
   } catch (err) {
     console.error(err);
@@ -60,10 +92,23 @@ async function editarUsuario(req, res){
   }
 }
 
-async function deletar(req, res) {
+
+
+
+async function deleteContaEmpresa(req, res) {
   const { id } = req.params;
   try {
-    await contasModel.deletar(id);
+    await contasModel.deleteContaEmpresa(id);
+    res.json({ msg: "Usuário deletado com sucesso" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Erro ao deletar usuário");
+  }
+}
+async function deleteContaHospital(req, res) {
+  const { id } = req.params;
+  try {
+    await contasModel.deleteContaHospital(id);
     res.json({ msg: "Usuário deletado com sucesso" });
   } catch (err) {
     console.error(err);
@@ -71,14 +116,16 @@ async function deletar(req, res) {
   }
 }
 
-function cadastrarFunc(req, res) {
+
+
+
+function cadastrarFuncHospital(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
     var perfil = req.body.perfilServer;
-    var fk_empresa = req.body.fkEmpresaServer;
-    var fk_hospital = req.body.fkHospitalServer;
+    var fkHospital = req.body.fkHospital;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -91,7 +138,43 @@ function cadastrarFunc(req, res) {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         
-        contasModel.cadastrarFunc(nome, email, senha, fk_empresa, fk_hospital, perfil)
+        contasModel.cadastrarFuncHospital(nome, email, senha, perfil, fkHospital)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+function cadastrarFuncEmpresa(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    var perfil = req.body.perfilServer;
+    var fk_empresa = req.body.fkEmpresaServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    }else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        
+        contasModel.cadastrarFuncEmpresa(nome, email, senha, fk_empresa, perfil)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -115,4 +198,4 @@ function listarHospitais(req, res){
     .catch(err => res.status(500).json(err))
 }
 
-module.exports = { listar, alterarAcesso, deletar, cadastrarFunc, listarHospitais, editarUsuario, listarHospital,listarEmpresa };
+module.exports = { listar, alterarAcessoEmpresa, alterarAcessoHospital, deleteContaEmpresa, deleteContaHospital, cadastrarFuncEmpresa, cadastrarFuncHospital, listarHospitais, editarContaEmpresa, editarContaHospital, listarHospital, listarEmpresa };
